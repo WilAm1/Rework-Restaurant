7cd24d7dee91810a4b81c0e7462c0c3adf9243dc
Nub <wilhelmjanmapogi@gmail.com>
2021-11-30 09:26:30 +0800

    Setup webpack config

---
+++ dist/main.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log('hello world');
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBLDJCIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vcmV3b3JrLXJlc3RhdXJhbnQvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc29sZS5sb2coJ2hlbGxvIHdvcmxkJyk7Il0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
