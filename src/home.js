450d872554af28c7129488ae6b231faa6ada53ac
Nub <wilhelmjanmapogi@gmail.com>
2021-11-30 10:18:18 +0800

    Add home code

---
+++ src/home.js
@@ -0,0 +1,9 @@
+export default function() {
+    const div = document.createElement('div');
+    div.id = "home";
+    div.innerHTML = `
+    
+    `;
+
+    return div
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 console.log('hello world');
+
+
+const config = {
+    content: document.querySelector('#content'),
+    home: document.querySelector('#home'),
+    menu: document.querySelector('#menu'),
+    contact: document.querySelector('#contact'),
+}
+
+const onLoad = function({ content, home, menu, contact }) {
+
+};
+
+
+document.onload = onLoad;
