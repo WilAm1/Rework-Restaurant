6e6c038b6a5536fb7adb7ac0dcc8e8cce0b0382d
Nub <wilhelmjanmapogi@gmail.com>
2021-12-01 08:48:08 +0800

    Add images to menu

--- src/menu.js
+++ src/menu.js
@@ -1,3 +1,20 @@
+const IMG_PATH = './images/Food/'
+
+const wrapDiv = (food, format) => {
+    const imageFile = `${IMG_PATH}${food}.${format}`;
+    const div = document.createElement('div');
+    div.classList.add('food');
+    div.innerHTML = `
+            <img id="${food}-img" src="${imageFile}">
+            <p>${food}</p>
+    `;
+    return div;
+
+}
+const beverageList = ['coke', 'pepsi', 'iced-tea', 'coffee'];
+const dishList = ['deluxe', 'ham-and-cheese', 'meat-supreme', 'vegan'];
+
+
 export default function() {
     const div = document.createElement('div');
     div.id = "home";
@@ -21,5 +38,11 @@ export default function() {
         </div>
     `;
 
+    const beverageContainer = div.querySelector('#beverage');
+    const dishContainer = div.querySelector('#main-menu');
+    const beverageDivs = beverageList.map((drink) => wrapDiv(drink, `jpg`));
+    const dishDivs = dishList.map((dish) => wrapDiv(dish, `png`));
+    beverageContainer.append(...beverageDivs);
+    dishContainer.append(...dishDivs);
     return div
 }
